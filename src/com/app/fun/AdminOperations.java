8d699daa65f9a5a57503816621e149dc1e737f9f
Naveen <naveenpuli123@outlook.com>
2022-12-04 16:20:12 +0000

    Unit testing for books & users

--- src/com/app/fun/AdminOperations.java
+++ src/com/app/fun/AdminOperations.java
@@ -16,22 +16,38 @@ public class AdminOperations {
 	
 	public static java.util.Date date = new java.util.Date();
 	
+	public static JOptionPane optionPane;
+	
+	public static JTable tableData;
+	public static JFrame dataFrame;
+	
 	//Enroll Student
 	public static JFrame enrollStudent;
 	public static JButton enrollStudentButton;
 	public static JTextField enrollStudentRoll, enrollStudentName;
+	
+	//Add Book
+	public static JFrame addFrame;
+	public static JButton addBookButton;
+	public static JTextField bookNameInput, bookAuthorInput, bookGenereInput, bookPriceInput;
+	
+	//Add User
+	public static JFrame addUserFrame;
+	public static JTextField userInput;
+	public static JPasswordField userPassword, userConfirmPassword;
+	public static JButton addUserButton;
 
 	public static void makeATableBoii(String[][] data, String[] cols, String title) {
 		
 		//JTable bookData = new JTable();
-		final JTable bookData = new JTable(data, cols);
+		tableData = new JTable(data, cols);
 		JScrollPane scrollPane = new JScrollPane();
-		JFrame dataFrame = new JFrame();
+		dataFrame = new JFrame();
 		
 		//bookData = new JTable(data, cols);
-		scrollPane = new JScrollPane(bookData);
-		bookData.setFont(new Font("Arial", Font.BOLD, 16));
-		bookData.setRowHeight(30);
+		scrollPane = new JScrollPane(tableData);
+		tableData.setFont(new Font("Arial", Font.BOLD, 16));
+		tableData.setRowHeight(30);
 		scrollPane.setBounds(0, 0, 800, 700);
 		//Library.newFrame(title, 800, 700);
 		//Library.frame.add(scrollPane);
@@ -42,39 +58,48 @@ public class AdminOperations {
 		dataFrame.add(scrollPane);
 		dataFrame.setVisible(true);
 		
-		bookData.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+		tableData.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
 			
 			@Override
 	        public void valueChanged(ListSelectionEvent event) {
-	            String id = bookData.getValueAt(bookData.getSelectedRow(), 0).toString();
-	            if(title.equalsIgnoreCase("Delete Books") || title.equalsIgnoreCase("Delete Users")) {
-	            	int n = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete ? ");
-	        		if (n == JOptionPane.YES_OPTION) {
+	            String id = tableData.getValueAt(tableData.getSelectedRow(), 0).toString();
+	            if(!id.equalsIgnoreCase(GeneralOperations.processedID)) {
+	            	if(title.equalsIgnoreCase("Delete Books")){
+			            GeneralOperations.confirmation("Delete Books","Are you sure you want to delete ?",id);
+			        }else if(title.equalsIgnoreCase("Delete Users")) {
+			            GeneralOperations.confirmation("Delete Users","Are you sure you want to delete ?",id);
+			        }else if(title.equalsIgnoreCase("Return Book")) {
+			            GeneralOperations.confirmation("Return Book","Are you sure you want to return this book ? ",id);
+			        }
+	            }
+	            /*if(title.equalsIgnoreCase("Delete Books") || title.equalsIgnoreCase("Delete Users")) {
+	            	//int n = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete ? ");
+	        		//if (n == JOptionPane.YES_OPTION) {
+	            	GeneralOperations.confirmation("Delete","Are you sure you want to delete ?");
+	            	if(GeneralOperations.confirmation("Are you sure you want to delete ?")) {
 	        			Connection connection = MySQLDriver.connect("root", "");
 	        			try {
 	        				Statement statement = connection.createStatement();
 	        				if(title.equalsIgnoreCase("Delete Books")) {
 	        					if(statement.executeUpdate("Delete from bookData where book_id=" + id) == 1) {
 	        						//System.out.println("Record Deleted");
-	        						JOptionPane.showMessageDialog(null, "Record Deleted", "Success",
-	        								JOptionPane.INFORMATION_MESSAGE);
+	        						//JOptionPane.showMessageDialog(null, "Record Deleted", "Success", JOptionPane.INFORMATION_MESSAGE);
+	        						GeneralOperations.alert("Success", "Record Deleted");
 	        						dataFrame.setVisible(false);
 	        						deleteBooks();
 	        					}else {
-	        						JOptionPane.showMessageDialog(null, "Failed to Delete", "Failed",
-	        								JOptionPane.INFORMATION_MESSAGE);
+	        						//JOptionPane.showMessageDialog(null, "Failed to Delete", "Failed", JOptionPane.INFORMATION_MESSAGE);
 	        						//System.out.println("Delete Failed");
+	        						GeneralOperations.alert("Error", "Failed to Delete");
 	        					}
 	        				}else if(title.equalsIgnoreCase("Delete Users")) {
 	        					if(statement.executeUpdate("Delete from users where id=" + id) == 1) {
 	        						//System.out.println("Record Deleted");
-	        						JOptionPane.showMessageDialog(null, "Record Deleted", "Success",
-	        								JOptionPane.INFORMATION_MESSAGE);
+	        						JOptionPane.showMessageDialog(null, "Record Deleted", "Success", JOptionPane.INFORMATION_MESSAGE);
 	        						dataFrame.setVisible(false);
 	        						deleteUsers();
 	        					}else {
-	        						JOptionPane.showMessageDialog(null, "Failed to Delete", "Failed",
-	        								JOptionPane.INFORMATION_MESSAGE);
+	        						JOptionPane.showMessageDialog(null, "Failed to Delete", "Failed", JOptionPane.INFORMATION_MESSAGE);
 	        						//System.out.println("Delete Failed");
 	        					}
 	        				}
@@ -88,83 +113,97 @@ public class AdminOperations {
 	        		if (n == JOptionPane.YES_OPTION) {
 	        			returnBook(id,dataFrame);
 	        		}
-	            }
+	            }*/
 	        }
 	    });
 		
 	}
 
 	public static void addBook() {
-		JFrame addFrame;
 		addFrame = Library.newJframeWindow("Add Book", 600, 400, JFrame.DISPOSE_ON_CLOSE);
 		JLabel name, auth, gen, price;
-		JTextField nameIN = null, authIN = null, genIN = null, priceIN = null;
-		JButton addBOOK = new JButton("Add Book");
-		addFrame.add(addBOOK);
-		addBOOK.setBounds(20, 300, 120, 30);
+		addBookButton = new JButton("Add Book");
+		addFrame.add(addBookButton);
+		addBookButton.setBounds(20, 300, 120, 30);
 
 		name = new JLabel("Enter Book's Name");
 		auth = new JLabel("Enter Book's Author");
 		gen = new JLabel("Enter Book's Genre");
-		price = new JLabel("Enter Book's Price");
+		price = new JLabel("Enter Book's Price (£)");
 
 		JLabel[] lables = { name, auth, gen, price };
-		String[] lableINPUT = new String[lables.length];
-		JTextField[] inputs = { nameIN, authIN, genIN, priceIN };
-
-		for (int i = 0; i < inputs.length; i++) {
-			inputs[i] = new JTextField();
-
-			addFrame.add(inputs[i]);
-		}
+		//String[] lableINPUT = new String[lables.length];
+		//JTextField[] inputs = { nameIN, authIN, genIN, priceIN };
+		bookNameInput = new JTextField();
+		bookAuthorInput = new JTextField();
+		bookGenereInput = new JTextField();
+		bookPriceInput = new JTextField();
+		
+		addFrame.add(bookNameInput);
+		addFrame.add(bookAuthorInput);
+		addFrame.add(bookGenereInput);
+		addFrame.add(bookPriceInput);
+		//for (int i = 0; i < inputs.length; i++) {
+		//	inputs[i] = new JTextField();
+		//	addFrame.add(inputs[i]);
+		//}
 
 		int yoff = 0;
 		for (int i = 0; i < lables.length; i++) {
-
-			lables[i].setBounds(20, 40 + yoff, 120, 20);
+			lables[i].setBounds(20, 40 + yoff, 180, 20);
 			addFrame.add(lables[i]);
-			inputs[i].setBounds(150, 40 + yoff, 400, 40);
-			inputs[i].setFont(new Font("Arial",Font.PLAIN,20));
+			//inputs[i].setBounds(150, 40 + yoff, 400, 40);
+			//inputs[i].setFont(new Font("Arial",Font.PLAIN,20));
 			yoff += 60;
 		}
 		
+		bookNameInput.setBounds(200,40,380,40);
+		bookNameInput.setFont(new Font("Arial",Font.PLAIN,20));
+		bookAuthorInput.setBounds(200,100,380,40);
+		bookAuthorInput.setFont(new Font("Arial",Font.PLAIN,20));
+		bookGenereInput.setBounds(200,160,380,40);
+		bookGenereInput.setFont(new Font("Arial",Font.PLAIN,20));
+		bookPriceInput.setBounds(200,220,380,40);
+		bookPriceInput.setFont(new Font("Arial",Font.PLAIN,20));
+		
 		addFrame.setVisible(true);
 
-		addBOOK.addActionListener(new ActionListener() {
+		addBookButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				lableINPUT[0] = inputs[0].getText();
-				lableINPUT[1] = inputs[1].getText();
-				lableINPUT[2] = inputs[2].getText();
-				lableINPUT[3] = inputs[3].getText();
+				//lableINPUT[0] = inputs[0].getText();
+				//lableINPUT[1] = inputs[1].getText();
+				//lableINPUT[2] = inputs[2].getText();
+				//lableINPUT[3] = inputs[3].getText();
 				boolean allow = true;
 				for (int i = 0; i < 4; i++) {
-					if (lableINPUT[i].isEmpty()) {
+					if (bookNameInput.getText().isEmpty() || bookAuthorInput.getText().isEmpty() || bookGenereInput.getText().isEmpty() || bookPriceInput.getText().isEmpty()) {
 						allow = false;
-						JOptionPane.showMessageDialog(null, "Please don't leave any field blank", "Error",
-								JOptionPane.ERROR_MESSAGE);
+						//JOptionPane.showMessageDialog(null, "Please don't leave any field blank", "Error", JOptionPane.ERROR_MESSAGE);
+						GeneralOperations.alert("Error", "Please don't leave any field blank");
 						break;
 					}
 				}
 				
 				try {
-					Float.parseFloat(lableINPUT[3]);
+					if(allow) {
+						Float.parseFloat(bookPriceInput.getText());
+					}
 				}catch(NumberFormatException numExep) {
 					allow = false;
-					JOptionPane.showMessageDialog(null, "Please enter numeric value for price", "Error",
-							JOptionPane.ERROR_MESSAGE);
+					//JOptionPane.showMessageDialog(null, "Please enter numeric value for price", "Error", JOptionPane.ERROR_MESSAGE);
+					GeneralOperations.alert("Error", "Please enter numeric value for price");
 				}
-				
 				
 				if (allow) {
 					Connection connection = MySQLDriver.connect("root", "");
-
+					//System.out.println("name = " + bookNameInput);
 					MySQLDriver.insertToTable(connection,
-							"insert into bookData(name,author,genre,price,issued) values('" + lableINPUT[0] + "','"
-									+ lableINPUT[1] + "','" + lableINPUT[2] + "','" + lableINPUT[3] + "',0)");
-					JOptionPane.showMessageDialog(null, "Book added to database", "Success",
-							JOptionPane.INFORMATION_MESSAGE);
+							"insert into bookData(name,author,genre,price,issued) values('" + bookNameInput.getText() + "','"
+									+ bookAuthorInput.getText() + "','" + bookGenereInput.getText() + "','" + bookPriceInput.getText() + "',0)");
+					//JOptionPane.showMessageDialog(null, "Book added to database", "Success", JOptionPane.INFORMATION_MESSAGE);
+					GeneralOperations.alert("Success", "Book added to database");
 					addFrame.setVisible(false);
 					try {
 						connection.close();
@@ -526,7 +565,7 @@ public class AdminOperations {
 		Uname = new JLabel("Student Name : ");
 
 		JLabel[] lables = { Uroll, Uname };
-		String[] lableINPUT = new String[lables.length];
+		//String[] lableINPUT = new String[lables.length];
 		//JTextField[] inputs = { enrollStudentRoll, enrollStudentName };
 
 		//for (int i = 0; i < inputs.length; i++) {
@@ -568,16 +607,31 @@ public class AdminOperations {
 					}
 				}
 				
+				try {
+					if(allow) {
+						Integer.parseInt(enrollStudentRoll.getText());
+					}
+				}catch(NumberFormatException numExep) {
+					allow = false;
+					//JOptionPane.showMessageDialog(null, "Please enter numeric value for price", "Error", JOptionPane.ERROR_MESSAGE);
+					GeneralOperations.alert("Error", "Please enter numeric value for roll number");
+				}
+				
 				if (allow) {
 					Connection connection = MySQLDriver.connect("root", "");
-
-					MySQLDriver.insertToTable(connection,
-							"insert into students(roll_number,student_name) values('" + lableINPUT[0] + "','"
-									+ lableINPUT[1] + "')");
-					//JOptionPane.showMessageDialog(null, "User Enrolled Successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
-					GeneralOperations.alert("Success", "User Enrolled Successfully");
-					enrollStudent.setVisible(false);
 					try {
+						Statement statement = connection.createStatement();	
+						ResultSet set1 = statement.executeQuery("Select * from students where roll_number=" + enrollStudentRoll.getText());
+						String studName = "";
+						if(set1.next() == true) {
+							//JOptionPane.showMessageDialog(null, "Student already registered", "Error", JOptionPane.ERROR_MESSAGE);	
+							GeneralOperations.alert("Error","Student already registered");
+						}else {
+							MySQLDriver.insertToTable(connection, "insert into students(roll_number,student_name) values('" + enrollStudentRoll.getText() + "','" + enrollStudentName.getText() + "')");
+							//JOptionPane.showMessageDialog(null, "User Enrolled Successfully", "Success", JOptionPane.INFORMATION_MESSAGE);
+							GeneralOperations.alert("Success", "User Enrolled Successfully");
+							enrollStudent.setVisible(false);
+						}
 						connection.close();
 						//System.out.println("Connection closed");
 					} catch (SQLException e1) {
@@ -736,64 +790,69 @@ public class AdminOperations {
 	}
 	
 	public static void addUser() {
-		JFrame AddUserFrame;
 		JLabel userN = new JLabel("Username");
 		JLabel userP = new JLabel("Password");
 		JLabel userConfP = new JLabel("Confirm Password");
-		JTextField userInN = new JTextField();
-		JPasswordField userConfInP = new JPasswordField();
-		JPasswordField userInP = new JPasswordField();
-		JButton addUser = new JButton("Add user");
+		
+		userInput = new JTextField();
+		userPassword = new JPasswordField();
+		userConfirmPassword = new JPasswordField();
+		
+		addUserButton = new JButton("Add user");
 
-		AddUserFrame = Library.newJframeWindow("Add a new user", 600, 310, JFrame.DISPOSE_ON_CLOSE);
+		addUserFrame = Library.newJframeWindow("Add a new user", 600, 310, JFrame.DISPOSE_ON_CLOSE);
 
 		userN.setBounds(30, 15, 100, 30);
 		userP.setBounds(30, 60, 100, 30);
 		userConfP.setBounds(30, 105, 150, 30);
-		userInN.setBounds(150, 15, 320, 40);
-		userInN.setFont(new Font("Arial",Font.PLAIN,20));
-		userInP.setBounds(150, 60, 320, 40);
-		userInP.setFont(new Font("Arial",Font.PLAIN,20));
-		userConfInP.setBounds(150, 105, 320, 40);
-		userConfInP.setFont(new Font("Arial",Font.PLAIN,20));
-		addUser.setBounds(130, 170, 120, 25);
+		userInput.setBounds(150, 15, 320, 40);
+		userInput.setFont(new Font("Arial",Font.PLAIN,20));
+		userPassword.setBounds(150, 60, 320, 40);
+		userPassword.setFont(new Font("Arial",Font.PLAIN,20));
+		userConfirmPassword.setBounds(150, 105, 320, 40);
+		userConfirmPassword.setFont(new Font("Arial",Font.PLAIN,20));
+		addUserButton.setBounds(130, 170, 120, 25);
 
-		AddUserFrame.add(userN);
-		AddUserFrame.add(userP);
-		AddUserFrame.add(userConfP);
-		AddUserFrame.add(userInN);
-		AddUserFrame.add(userInP);
-		AddUserFrame.add(userConfInP);
-		AddUserFrame.add(addUser);
-		
-		AddUserFrame.setVisible(true);
+		addUserFrame.add(userN);
+		addUserFrame.add(userP);
+		addUserFrame.add(userConfP);
+		addUserFrame.add(userInput);
+		addUserFrame.add(userPassword);
+		addUserFrame.add(userConfirmPassword);
+		addUserFrame.add(addUserButton);	
+		addUserFrame.setVisible(true);
 
-		addUser.addActionListener(new ActionListener() {
+		addUserButton.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				String uname = userInN.getText();
-				String upass = String.valueOf(userInP.getPassword());
-				String cupass = String.valueOf(userConfInP.getPassword());
-				if (uname.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Please Provide username");
-				} else if (upass.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Please Provide password");
-				} else if (cupass.isEmpty()) {
-					JOptionPane.showMessageDialog(null, "Please Confirm password");
-				} else if (!cupass.equals(upass)) {
-					JOptionPane.showMessageDialog(null, "Please Enter password correctly");
+				//String uname = userInN.getText();
+				//String upass = String.valueOf(userInP.getPassword());
+				//String cupass = String.valueOf(userConfInP.getPassword());
+				if (userInput.getText().isEmpty() || String.copyValueOf(userPassword.getPassword()).isEmpty() || String.copyValueOf(userConfirmPassword.getPassword()).isEmpty()){
+					//JOptionPane.showMessageDialog(null, "Please Provide username");
+					GeneralOperations.alert("Error", "Please don't leave any field blank");
+				} else if (!String.copyValueOf(userPassword.getPassword()).equals(String.copyValueOf(userConfirmPassword.getPassword()))) {
+					//JOptionPane.showMessageDialog(null, "Please Enter password correctly");
+					GeneralOperations.alert("Success","Please Enter password correctly");
 				} else {
 					Connection connection = MySQLDriver.connect("root", "");
-					String query = "insert into users(username,password) values('" + uname + "' , '" + upass + "')";
-					JOptionPane.showMessageDialog(null,"User Added");
-					MySQLDriver.insertToTable(connection, query);
-					
-					AddUserFrame.setVisible(false);
-					AddUserFrame.dispose();
-					
 					try {
+						Statement statement = connection.createStatement();	
+						ResultSet set1 = statement.executeQuery("Select * from users where username='" + userInput.getText() +"'");
+						String studName = "";
+						if(set1.next() == true) {
+							//JOptionPane.showMessageDialog(null, "Student already registered", "Error", JOptionPane.ERROR_MESSAGE);	
+							GeneralOperations.alert("Error","User already exists");
+						}else {
+							String query = "insert into users(username,password) values('" + userInput.getText() + "' , '" + String.copyValueOf(userConfirmPassword.getPassword()) + "')";
+							//JOptionPane.showMessageDialog(null,"User Added");
+							GeneralOperations.alert("Success","User Added");
+							MySQLDriver.insertToTable(connection, query);
+							addUserFrame.setVisible(false);
+							addUserFrame.dispose();
+						}
 						connection.close();
 					} catch (SQLException e1) {
 						// TODO Auto-generated catch block
--- src/com/app/fun/GeneralOperations.java
+++ src/com/app/fun/GeneralOperations.java
@@ -2,8 +2,10 @@ package com.app.fun;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -24,12 +26,14 @@ import com.app.ui.Library;
 public class GeneralOperations {
 	
 	public static JFrame alertFrame;
-	public static JFrame infoFrame;
 	public static JFrame optionFrame;
 	public static JButton okButton;
 	public static JButton yesButton;
 	public static JButton noButton;
 	public static JLabel message;
+	
+	public static String processedID = "";
+	
 	
 	public static void alert(String alertType, String messageText) {
 		okButton = new JButton("OK");
@@ -37,7 +41,7 @@ public class GeneralOperations {
 		
 		message = new JLabel(messageText);
 		message.setForeground(Color.black);
-		message.setFont(new Font("Arial", Font.BOLD, 20));
+		message.setFont(new Font("Arial", Font.BOLD, 15));
 		message.setBounds(20, 10, 300, 80);
 		
 		if(alertType.equalsIgnoreCase("Error")) {
@@ -58,8 +62,91 @@ public class GeneralOperations {
 			public void actionPerformed(ActionEvent e) {
 				alertFrame.setVisible(false);
 				alertFrame.dispose();
+				if(messageText.equalsIgnoreCase("Book Deleted")) {
+					AdminOperations.dataFrame.setVisible(false);
+					AdminOperations.deleteBooks();
+				}else if(messageText.equalsIgnoreCase("User Deleted")) {
+					AdminOperations.dataFrame.setVisible(false);
+					AdminOperations.deleteUsers();
+				}
 			}
 		});
+	}
+	
+	public static void confirmation(String title, String messageText, String id) {
+		processedID = id;
+		yesButton = new JButton("Yes");
+		yesButton.setBounds(20, 80, 80, 25);
+		
+		noButton = new JButton("No");
+		noButton.setBounds(100, 80, 80, 25);
+		
+		message = new JLabel(messageText);
+		message.setForeground(Color.black);
+		message.setFont(new Font("Arial", Font.BOLD, 15));
+		message.setBounds(20, 10, 300, 80);
+		
+		optionFrame = Library.newJframeWindow(title, 350, 150, JFrame.DISPOSE_ON_CLOSE);
+		optionFrame.add(message);
+		optionFrame.add(yesButton);
+		optionFrame.add(noButton);
+		
+		optionFrame.setVisible(true);
+		
+		yesButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				AdminOperations.dataFrame.setVisible(false);
+				AdminOperations.dataFrame.dispose();
+				optionFrame.setVisible(false);
+				optionFrame.dispose();
+				Connection connection = MySQLDriver.connect("root", "");	
+				try {
+    				Statement statement = connection.createStatement();
+    				if(title.equalsIgnoreCase("Delete Books")) {
+    					if(statement.executeUpdate("Delete from bookData where book_id=" + id) == 1) {
+    						alert("Success", "Book Deleted");
+    					}else {
+    						alert("Error", "Failed to Delete Book");
+    					}
+    				}else if(title.equalsIgnoreCase("Delete Users")) {
+    					if(statement.executeUpdate("Delete from users where id=" + id) == 1) {
+    						alert("Success", "User Deleted");
+    					}else {
+    						alert("Error", "Failed to Delete User");
+    					}
+    				}else if(title.equalsIgnoreCase("Return Book")) {
+    					AdminOperations.returnBook(id,AdminOperations.dataFrame);
+    				}
+					
+    			} catch (Exception e1) {
+    				e1.printStackTrace();
+    			}
+			}
+		});
+		
+		noButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//System.out.println("Clicked No Button");
+				AdminOperations.dataFrame.setVisible(false);
+				AdminOperations.dataFrame.dispose();
+				optionFrame.setVisible(false);
+				optionFrame.dispose();
+				if(title.equalsIgnoreCase("Delete Books")) {
+					processedID = "";
+					AdminOperations.deleteBooks();
+				} else if(title.equalsIgnoreCase("Delete Users")) {
+					processedID = "";
+					AdminOperations.deleteUsers();
+				} else if(title.equalsIgnoreCase("Return Book")) {
+					processedID = "";
+					AdminOperations.returnBook(id, AdminOperations.dataFrame);
+				}
+				
+			}
+		});
+		
 	}
 
 }
